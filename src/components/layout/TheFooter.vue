b5c482f3624c05980115aade8b6998699786941d
Jody Geraldo <ajody.geraldo@gmail.com>
2020-10-30 17:21:49 +0700

    using dynamic year for copyright, remove brand link

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,17 +1,23 @@
 <template>
   <b-navbar>
-    <b-navbar-brand :to="{ name: 'Classic' }">JodyCryptoVue</b-navbar-brand>
+    <b-navbar-brand>JodyCryptoVue</b-navbar-brand>
 
     <b-navbar-nav class="ml-auto">
-      <b-nav-text>Copyright &copy; 2020 Jody Geraldo</b-nav-text>
+      <b-nav-text>Copyright &copy; {{ year }} Jody Geraldo</b-nav-text>
     </b-navbar-nav>
   </b-navbar>
 </template>
 
 <script>
 export default {
-  name: 'TheFooter'
+  name: 'TheFooter',
+  data() {
+    return {
+      year: null
+    }
+  },
+  mounted() {
+    this.year = new Date().getFullYear()
+  }
 }
 </script>
-
-<style lang="scss" scoped></style>
