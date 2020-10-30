f5fbc173dde43328f2dcf17bbb9d4936f9a25fd3
Jody Geraldo <ajody.geraldo@gmail.com>
2020-10-30 23:02:36 +0700

    bootstrap treeshaking with component groups import

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
 
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+  LayoutPlugin,
+  CardPlugin,
+  NavPlugin,
+  NavbarPlugin,
+  FormPlugin,
+  FormGroupPlugin,
+  FormInputPlugin,
+  FormTextareaPlugin,
+  AlertPlugin,
+  ButtonPlugin
+} from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
+Vue.use(LayoutPlugin)
+Vue.use(CardPlugin)
+Vue.use(NavPlugin)
+Vue.use(NavbarPlugin)
+Vue.use(FormPlugin)
+Vue.use(FormGroupPlugin)
+Vue.use(FormInputPlugin)
+Vue.use(FormTextareaPlugin)
+Vue.use(AlertPlugin)
+Vue.use(ButtonPlugin)
